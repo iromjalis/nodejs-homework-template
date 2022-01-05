f9a24a4656d5a75d168c78c622f6e7c40966a3bb
Irina Romjalis <iromjalis@gmail.com>
2022-01-06 01:40:55 +0200

    +

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -19,24 +19,4 @@ router.put("/:id", ctrlWrapper(ctrl.updateContact));
 
 router.delete("/:contactId", ctrlWrapper(ctrl.removeContact));
 
-// router.put("/:contactId", async (req, res, next) => {
-//   try {
-//     const { contactId } = req.params;
-//     console.log("contactId: ", contactId);
-
-//     const updateContact = await contactsOperations.updateFunction(
-//       contactId,
-//       req.body
-//     );
-
-//     if (!updateContact) {
-//       throw new NotFound();
-//     }
-
-//     res.json(updateContact);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
 module.exports = router;
