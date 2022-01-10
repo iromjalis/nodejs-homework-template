0aa636c7d995d18a5188f6bf53322e0e5e0a261f
Irina Romjalis <iromjalis@gmail.com>
2022-01-10 20:38:56 +0200

    hw5 done

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+// const multer = require("multer");
+// const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const contactsRouter = require('./routes/api/contacts')
+const authRouter = require("./routes/api/auth");
+const usersRouter = require("./routes/api/users");
+const contactsRouter = require("./routes/api/contacts");
+const upload = require("./middlewares/upload");
+const app = express();
 
-const app = express()
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+// app.use("/api/users", authRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter);
 
-app.use('/api/contacts', contactsRouter)
+app.post("/api/contacts", upload.single("image"), async (req, res) => {
+  const { path: tempUpload, originalname } = req.file;
+  const resultUpload = path.join(productsDir, originalname);
+  try {
+    await fs.rename(tempUpload, resultUpload);
+    const image = path.join("contacts", originalname);
+    const newContact = {
+      name: req.body.name,
+      id: v4(),
+      image,
+    };
+    products.push(newContact);
 
+    res.status(201).json(newContact);
+  } catch (error) {
+    await fs.unlink(tempUpload);
+  }
+});
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
